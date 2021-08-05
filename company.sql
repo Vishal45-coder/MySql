a2dd17028f5fc93269121e8d6ee3ee2fa21d707f
Vishal45-coder <vishalraavi1999@gmail.com>
2021-08-05 16:32:00 +0530

    Added different statements and made some changes

--- company.sql
+++ company.sql
@@ -1,31 +1,42 @@
 create database company;
 use company;
-create table Employee(id int,Name varchar(20),Age int,Gender varchar(2),Salary double);
+create table Employee(emp_id int,Name varchar(20),Age int,Gender varchar(2),Salary double);
 
-create table Departments(Name varchar(20),Dept_id int,Dept_Name varchar(30));
+create table Departments(emp_id int,Dept_id varchar(10),Dept_Name varchar(30));
 
 show tables;
 
 
-insert into Employee(id,Name,Age,Gender,Salary) values(567983,"Joey",30,"M",65600);
+insert into Employee(emp_id,Name,Age,Gender,Salary) values(567983,"Joey",30,"M",65600);
 
-insert into Employee(id,Name,Age,Gender,Salary) values(567988,"Ross",31,"M",90600);
+insert into Employee(emp_id,Name,Age,Gender,Salary) values(567988,"Ross",31,"M",90600);
 
-insert into Employee(id,Name,Age,Gender,Salary) values(567987,"Chandler",33,"M",70600);
+insert into Employee(emp_id,Name,Age,Gender,Salary) values(567987,"Chandler",33,"M",70600);
 
+
+insert into Departments(emp_id,Dept_id,Dept_name) values (567893,"QWEDF45","Accountant");
+
+insert into Departments(emp_id,Dept_id,Dept_name) values (567898,"QWEDF75","Biotechnoogy");
+
+insert into Departments(emp_id,Dept_id,Dept_name) values (567897,"QWEDF95","Recruiter");
 /**
 Here we use (*) because we want to select all the attributes from the table Employee
 */
 select* from Employee; 
-
+select* from Departments;
 /**
 Here we use Name,id,Salary attribute because we want to get Name,id,Salary attribute from Employee table
 */
 select Name from Employee;
-select id from Employee;
+select emp_id from Employee;
 select Salary from Employee;
 
 /**
 To select two attributes at a  time from a same table
 */
-select Name,id from Employee;
+select Name,emp_id from Employee;
+
+/**
+This is used when we want to select all attributes from n tables
+*/
+select* from Employee,Departments;
