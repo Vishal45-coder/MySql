b39ad241d7b0329ad9a996ffa5124b29826b62e2
Vishal45-coder <vishalraavi1999@gmail.com>
2021-08-19 15:52:58 +0530

    Created database with Constraints

---
+++ Constraints.sql
@@ -0,0 +1,17 @@
+create database student;
+use student;
+create table student
+(
+     ID int not null,
+     FirstName varchar(255),
+     LastName varchar(255),
+     Age int not null check(Age>17),
+     unique(ID)
+);
+insert into student(ID,FirstName,LastName,Age) values (1,"Joey","Fino",18);
+insert into student(ID,FirstName,LastName,Age) values (2,"Ross","Dino",18);
+insert into student(ID,FirstName,LastName,Age) values (3,"Chandler","laug",19);
+--THis below will not execute because the age is less than the constraint value
+insert into student(ID,FirstName,LastName,Age) values (4,"Rachel","",16);
+
+select *from student;
